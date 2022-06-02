f465352670051416913b5f8c0389f2b9fd5ab6a1
RelancioBorgesDev <relancio.dev@gmail.com>
2022-06-02 12:17:17 -0300

    Adicionado Pagina de Login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,15 +1,38 @@
 import React from 'react'
-import { Container } from './style'
-
+import { Container, InputContainer, GoogleIcon, LoginContainer, LoginGoogleBtn, Titulo, CheckBoxContainer, LoginBtn, HaveAccount } from './style'
+import Wallpaper from '../../assets/nftWallpaper.jpg'
 export default function Login() {
   return (
     <Container>
       <div>
-        Imagem
+        <img src={Wallpaper} alt="Login Image" width="100%" height="100%"/>
       </div>
-      <div>
-        Input
-      </div>
+      <LoginContainer>
+        <Titulo>Login</Titulo>
+        <LoginGoogleBtn>
+          <GoogleIcon/>
+          Sign in with Google
+          </LoginGoogleBtn>
+        <InputContainer>
+          <label htmlFor="">Email:</label>
+          <input type="email" placeholder="Type your email...."/>
+        </InputContainer>
+        <InputContainer>
+          <label htmlFor="">Password:</label>
+          <input type="password" placeholder="Type your password...."/>
+        </InputContainer>
+
+        <CheckBoxContainer>
+          <div>
+            <input type="checkbox" />
+            <label htmlFor="">Remember Me</label>
+          </div>
+          <a href="#">Forgot Password ?</a>
+        </CheckBoxContainer>
+
+        <LoginBtn>Log In</LoginBtn>
+        <HaveAccount>Don't have an account?<a href="#">Sign UP</a></HaveAccount>
+      </LoginContainer>
     </Container>
   )
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,6 +1,129 @@
 import styled from 'styled-components';
+import {Google} from '../../styles/Icons'
 
 export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50%;
+  grid-template-areas:
+  "imagem login_container"
+  ;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #1d1254;
+
+  @media (max-width:975px){
+    background-image: linear-gradient(to top, #17191a, #041c2a, #001d3c, #001b4b, #1d1254);
+    display: flex;
+    > div img {
+      display: none;
+    }
+  }
+
+`
+export const LoginContainer = styled.div`
+  width: 60%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
 
 
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+
+`
+
+export const Titulo = styled.h1`
+  width: 100%;
+  text-align: center;
+  color: #f5f5f5;
+  font-weight: bold;
+  border-bottom: 1px solid #fff;
+  padding: 1rem;
+`
+export const LoginGoogleBtn = styled.button`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  border: 4px solid #2f1655;
+  background-color: #17191a;
+  color: #f5f5f5;
+  font-weight: lighter;
+  padding: 1rem;
+  cursor: pointer;
+
+`
+export const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: .5rem;
+
+  > label{
+    color: #f5f5f5;
+    font-weight: bold;
+  }
+  > input {
+    width: 100%;
+    background-color: #17191a;
+    color: #f5f5f5;
+    padding: 1rem;
+    outline: 0;
+  }
+
+`
+
+export const CheckBoxContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #fff;
+  font-weight: 500;
+
+  > div{
+    display: flex;
+    gap: .5rem;
+  }
+
+  > a {
+    color: #563864;
+  }
+
+`
+
+export const LoginBtn = styled.button`
+  width: 100%;
+  border: none;
+  border-radius: .2rem;
+  font-size: 1.4rem;
+  color: #fff;
+  font-weight: bold;
+  padding: 1rem;
+  background-color: #6720ff;
+
+`
+
+export const HaveAccount = styled.span`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .2rem;
+  color: #fff;
+  font-weight: 500;
+  > a {
+    color: #563864;
+  }
+`
+
+export const GoogleIcon = styled(Google)`
+  width:20px;
+  height:20px;
 `
